8b2d8753a941e93623ca4d766b629e898eac1444
Gavin Liu <du20783@gmail.com>
2020-08-25 22:00:23 +0800

    add model Comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  has_many :posts
+  has_many :comments
 end
