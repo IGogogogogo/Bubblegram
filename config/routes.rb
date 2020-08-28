80b26275f5753a561af753996daa82f1390b3b00
Gavin Liu <du20783@gmail.com>
2020-08-28 16:38:46 +0800

    add routes follows

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show] do 
+    resource :follows, only: [:create, :destroy]
+    
+    member do
+      get :fans 
+      get :followings
+    end
+  end
+
 
   resources :posts do 
     resources :comments, only: [:create]
