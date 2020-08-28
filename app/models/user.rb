3708f59099a5d8ee0bcb39c82822b21bad1c1f18
Gavin Liu <du20783@gmail.com>
2020-08-28 22:27:47 +0800

    add validates for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  validates :nick_name, presence: true
+  validates :email, presence: true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
+  has_many :posts
+  has_many :comments
   has_many :followingships, foreign_key: :following_id, class_name: "Follow"
   has_many :fans, through: :followingships, source: :fan
 
