2f594c3ec6149c0d1b2f9949c9a2860208e46563
Gavin Liu <du20783@gmail.com>
2020-08-28 14:28:13 +0800

    add model Follow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_023027) do
+ActiveRecord::Schema.define(version: 2020_08_28_062400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(version: 2020_08_25_023027) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "fan_id"
+    t.integer "following_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["fan_id"], name: "index_follows_on_fan_id"
+    t.index ["following_id"], name: "index_follows_on_following_id"
   end
 
   create_table "posts", force: :cascade do |t|
