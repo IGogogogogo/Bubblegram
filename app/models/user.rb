11f6834b4791718f30fc384f5af807642f08d53f
Gavin Liu <du20783@gmail.com>
2020-08-28 14:52:10 +0800

    add user followship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
-  has_many :posts
-  has_many :comments
-end
+  has_many :followingships, foreign_key: :following_id, class_name: "Follow"
+  has_many :fans, through: :followingships, source: :fan
+
+  has_many :fanships, foreign_key: :fan_id, class_name: "Follow"
+  has_many :followings, through: :fanships, source: :following
+ end
