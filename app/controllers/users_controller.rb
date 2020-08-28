8adc6a51a7337a4133c5f98b414d754349a57f86
Gavin Liu <du20783@gmail.com>
2020-08-28 22:28:31 +0800

    add edit and update action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:show, :fans, :followings, :is_followings]
+  before_action :find_user, only: [:show, :fans, :followings, :edit, :update]
+  before_action :only_owner, only: [:edit, :update]
 
   helper_method :already_followed
 
@@ -14,13 +15,34 @@ class UsersController < ApplicationController
     @users = @user.followings
   end
 
+  def edit 
+  end
+
+  def update 
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "資料更新成功"
+    else
+      render :edit
+    end
+  end
+
   private 
 
   def find_user
     @user = User.find(params[:id])
   end
 
+  def user_params
+    params.require(:user).permit(:nick_name, :email)  
+  end
+
   def already_followed(user)                  #檢查自己是否已經追蹤對方
     user.fans.include?(current_user)
+  end
+
+  def only_owner
+    if current_user != @user                   #只有本人可以編輯個人資料
+      redirect_to user_path(@user.id), alert: "Not allow"
+    end
   end
 end
