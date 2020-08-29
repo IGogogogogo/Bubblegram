11ed240605533c5432a627254507121b3b3f07e5
Gavin Liu <du20783@gmail.com>
2020-08-29 13:21:26 +0800

    add User destroy relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
 class User < ApplicationRecord
   validates :nick_name, presence: true
   validates :email, presence: true
-  
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :trackable
 
   has_many :posts
   has_many :comments
-  has_many :followingships, foreign_key: :following_id, class_name: "Follow"
+  has_many :followingships, foreign_key: :following_id, class_name: "Follow", dependent: :destroy
   has_many :fans, through: :followingships, source: :fan
-
-  has_many :fanships, foreign_key: :fan_id, class_name: "Follow"
+  has_many :fanships, foreign_key: :fan_id, class_name: "Follow", dependent: :destroy
 	has_many :followings, through: :fanships, source: :following
 end
