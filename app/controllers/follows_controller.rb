5ee30a4ee3db62af504c9d0bb36450474329d59c
Gavin Liu <du20783@gmail.com>
2020-08-31 11:41:21 +0800

    add space to last row

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,19 +1,19 @@
 class FollowsController < ApplicationController
   before_action :find_user, only: [:create, :destroy]
 
-  def create 
+  def create
     @user.fans << current_user          #把自己加入對方的粉絲中
     redirect_to user_path(@user)
   end
 
-  def destroy 
+  def destroy
     @user.fans.destroy(current_user)    #把自己從對方的粉絲中刪除
     redirect_to user_path(@user)
   end
 
-  private 
+  private
 
-  def find_user 
+  def find_user
     @user = User.find(params[:user_id])
   end
 end
