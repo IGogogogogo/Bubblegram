a451f002e6eaac5c98804d1c1f1afb539cb1d87d
Gavin Liu <du20783@gmail.com>
2020-09-02 11:51:58 +0800

    add search routes and html

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,23 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
+  resources :pages, only: [:index] do
+    collection do
+      get :search
+    end
+  end
+
   devise_for :users
-  resources :users, only: [:show, :edit, :update] do 
+  resources :users, only: [:show, :edit, :update] do
     resource :follows, only: [:create, :destroy]
-    
+
     member do
-      get :fans 
+      get :fans
       get :followings
     end
   end
 
-
-  resources :posts do 
+  resources :posts do
     resources :comments, only: [:create]
   end
 end
