25d5e5b17cd17128ab1ee33d5a9187a367ff5993
Gavin Liu <du20783@gmail.com>
2020-09-02 15:47:45 +0800

    remove auth column from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_064304) do
+ActiveRecord::Schema.define(version: 2020_09_02_074046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,10 +103,6 @@ ActiveRecord::Schema.define(version: 2020_09_02_064304) do
     t.integer "collections_count", default: 0
     t.string "avatar"
     t.text "description"
-    t.string "fb_uid"
-    t.string "fb_token"
-    t.string "google_uid"
-    t.string "google_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
