ebcbe7235c538580aa6837edd8edcd6c49fa0c0b
chiayinin <chiayinin@gmail.com>
2020-09-02 11:37:16 +0800

    add_modle_done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,71 @@ class User < ApplicationRecord
   validates :nick_name, presence: true
   validates :email, presence: true
 
-  devise :database_authenticatable, :registerable, :recoverable,
-         :rememberable, :validatable, :trackable, :omniauthable,
-         :omniauth_providers: [:facebook, :google_oauth2]
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :trackable,
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   has_many :posts
   has_many :comments
   has_many :followingships, foreign_key: :following_id, class_name: "Follow", dependent: :destroy
   has_many :fans, through: :followingships, source: :fan
   has_many :fanships, foreign_key: :fan_id, class_name: "Follow", dependent: :destroy
-	has_many :followings, through: :fanships, source: :following
+  has_many :followings, through: :fanships, source: :following
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:google_token => access_token.credentials.token, :google_uid => access_token.uid ).first
+    if user
+      return user
+    else
+      existing_user = User.where(:email => data["email"]).first
+      if  existing_user
+        existing_user.google_uid = access_token.uid
+        existing_user.google_token = access_token.credentials.token
+        existing_user.save!
+        return existing_user
+      else
+    # Uncomment the section below if you want users to be created if they don't exist
+        user = User.create(
+            nick_name: data["name"],
+            email: data["email"],
+            password: Devise.friendly_token[0,20],
+            google_token: access_token.credentials.token,
+            google_uid: access_token.uid
+          )
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    # Case 1: Find existing user by facebook uid
+    user = User.find_by_fb_uid( auth.uid )
+    if user
+       user.fb_token = auth.credentials.token
+       #user.fb_raw_data = auth
+       user.save!
+      return user
+    end
+
+    # Case 2: Find existing user by email
+    existing_user = User.find_by_email( auth.info.email )
+    if existing_user
+      existing_user.fb_uid = auth.uid
+      existing_user.fb_token = auth.credentials.token
+      #existing_user.fb_raw_data = auth
+      existing_user.save!
+      return existing_user
+    end
+
+    # Case 3: Create new password
+    user = User.new
+    user.fb_uid = auth.uid
+    user.fb_token = auth.credentials.token
+    user.nick_name = auth.info.email.split('@')[0].capitalize
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    #user.fb_raw_data = auth
+    user.save!
+    return user
+  end
 end
