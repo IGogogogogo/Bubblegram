faee5131fa549204159f1d4eeb80a442b0cebe52
will <will@qiuyanyande-MacBook-Pro.local>
2020-09-02 20:50:31 +0800

    established model relation

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,8 @@
 class Chat < ApplicationRecord
+    validates :sender_id, uniqueness: { scope: :recipient_id }
+
+    has_many :messages, dependent: :destroy
+
+    belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
+    belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,8 @@ class User < ApplicationRecord
   has_many :followingships, foreign_key: :following_id, class_name: "Follow", dependent: :destroy
   has_many :fans, through: :followingships, source: :fan
   has_many :fanships, foreign_key: :fan_id, class_name: "Follow", dependent: :destroy
-	has_many :followings, through: :fanships, source: :following
+  has_many :followings, through: :fanships, source: :following
+  has_many :sender_chats, foreign_key: :sender_id, class_name: 'Chat'
+  has_many :recipient_chats, foreign_key: :recipient_id, class_name: 'Chat'
+  has_many :messages
 end
