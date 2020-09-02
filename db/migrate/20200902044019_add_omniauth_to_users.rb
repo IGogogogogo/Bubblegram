46faf6a8d89066b96b930835c2268eeb87566a3c
chiayinin <chiayinin@gmail.com>
2020-09-02 13:35:08 +0800

    add_OAuth_inusers

--- db/migrate/20200902044019_add_omniauth_to_users.rb
+++ db/migrate/20200902044019_add_omniauth_to_users.rb
@@ -1,4 +1,4 @@
-class AddOmniauthToUsers < ActiveRecord::Migration
+class AddOmniauthToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :fb_uid, :string
     add_column :users, :fb_token, :string
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_031517) do
+ActiveRecord::Schema.define(version: 2020_09_02_044019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,10 @@ ActiveRecord::Schema.define(version: 2020_08_31_031517) do
     t.integer "collections_count", default: 0
     t.string "avatar"
     t.text "description"
+    t.string "fb_uid"
+    t.string "fb_token"
+    t.string "google_uid"
+    t.string "google_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
