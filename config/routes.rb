9fffba12a6adbb0b977225bdd4a88b17f2765611
will <will@qiuyanyande-MacBook-Pro.local>
2020-09-02 21:12:07 +0800

    established chats nad messages routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,21 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   devise_for :users
-  resources :users, only: [:show, :edit, :update] do 
+  resources :users, only: [:show, :edit, :update] do
     resource :follows, only: [:create, :destroy]
-    
+
     member do
-      get :fans 
+      get :fans
       get :followings
     end
   end
 
 
-  resources :posts do 
+  resources :posts do
     resources :comments, only: [:create]
+  end
+
+  resources :chats, only: [:index, :show, :create, :destroy] do
+    resources :messages, only: [:create]
   end
 end
