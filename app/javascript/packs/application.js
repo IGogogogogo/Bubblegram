0f329a57538ec23353138081b50e24395445ca9d
陳柏蓉 <bobo@chenbairongde-MacBook-Pro.local>
2020-09-05 01:22:22 +0800

    add_preview_and_navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,17 @@ require("channels")
 
 require("trix")
 require("@rails/actiontext")
+import "bootstrap";
+import "../stylesheets/application"
+import $ from "jquery"
+import "@fortawesome/fontawesome-free/js/all";
+import "./preview_photo"
+
+window.$ = $
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
+
+
