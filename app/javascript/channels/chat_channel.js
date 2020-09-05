89525f93014485d3d35f7b55777b71ba444da23e
will <wye139629@gmail.com>
2020-09-05 12:05:40 +0800

    modified message other/me css and chat_channel.js const position

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -2,20 +2,20 @@ import consumer from "./consumer"
 
 document.addEventListener('turbolinks:load',()=>{
   const chat_id = document.querySelector('.message_box').dataset.chat
-  const user_id = Number(document.querySelector('.message_box').dataset.user)
-  const message_text_area = document.querySelector('.message_text_area')
-  const input = document.getElementById('message_content')
   consumer.subscriptions.create({channel:"ChatChannel", chat_id: chat_id}, {
-   connected() {
-     console.log('connecting'+ chat_id)
-     // Called when the subscription is ready for use on the server
-   },
+    connected() {
+      console.log('connecting'+ chat_id)
+      // Called when the subscription is ready for use on the server
+    },
 
     disconnected() {
       // Called when the subscription has been terminated by the server
     },
 
     received(data) {
+      const user_id = Number(document.querySelector('.message_box').dataset.user)
+      const message_text_area = document.querySelector('.message_text_area')
+      const input = document.getElementById('message_content')
       console.log(data)
 
       if(data.message.user_id === user_id){
@@ -25,6 +25,7 @@ document.addEventListener('turbolinks:load',()=>{
       }
 
       input.value=''
+      message_text_area.scrollTop = message_text_area.scrollHeight
         // Called when there's incoming data on the websocket for this channel
     }
   });
