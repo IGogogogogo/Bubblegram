6342246e3d6a63b93be36e68a2fe2bdb893f0528
Gavin Liu <du20783@gmail.com>
2020-09-12 23:30:46 +0800

    add pages welcome and update posts routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:welcome]
 
   def index
+    users = [current_user].concat(current_user.followings)
+    @posts = Post.viewable_posts(users).order("created_at DESC").limit(20)
+  end
+
+  def welcome
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
+  resources :pages, only: [:index] do
+    collection do
+      get :welcome
+    end
+  end
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   resources :users, only: [:show, :edit, :update] do
     resource :follows, only: [:create, :destroy]
+
+    resources :posts, shallow: true do
+      resources :comments, only: [:create]
+      collection do
+        get :load
+      end
+    end
 
     member do
       get :fans
@@ -19,7 +31,4 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :posts do
-    resources :comments, only: [:create]
-  end
 end
