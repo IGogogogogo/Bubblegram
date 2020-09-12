a0ffc77f66044725f2b65f5e321e16994c29cf88
Guest5x <guest5x@Hsuande-MacBook-Air.local>
2020-09-12 11:10:33 +0800

    create_routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :edit, :update] do
     resource :follows, only: [:create, :destroy]
+    resources :stories, only: [:index, :show, :new, :create, :destroy ]
 
     member do
       get :fans
