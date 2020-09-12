9c7ad3d79f258a0dd3e582aa72b59619046e7305
陳柏蓉 <jerry19920702@gmail.com>
2020-09-12 14:16:44 +0800

    comments move not yet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,12 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.with_rich_text_body #with_rich_text_body 是避免N+1方法
+    @comment = Comment.new
+    #@comments = @post.comments
   end
 
   def show
     @comments = @post.comments
-    @comment = Comment.new
   end
 
   def new
