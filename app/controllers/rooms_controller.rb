d069a9dbed0a68d977f4eee7e31daeaf53498407
gn2481 <gn2481@gmail.com>
2020-09-15 15:25:07 +0800

    1. add_rooms_controller_action (50%)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,30 @@
 class RoomsController < ApplicationController
+  # 直播間的rooms
+  before_action :find_room, only: %i[show play destroy]
+  def show
+  
+  end
+
+  def create
+    @room = Room.create
+    redirect to play_room_path(@room)
+  end
+
+  def play
+   
+  end
+
+  def destory
+  
+  end
+
+  private
+
+  def find_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit
+  end
 end
