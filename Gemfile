72d02a723e19132c74ff1dd83c483c2f00fe8282
gn2481 <gn2481@gmail.com>
2020-09-15 20:18:29 +0800

    add_openTok_api_gem

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem 'omniauth-google-oauth2', '~> 0.8.0'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'figaro', '~> 1.0'
 
+# Vonage api openTook gem
+gem "opentok", "~> 4.0.0"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,38 +4,39 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable,
-         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
-  mount_uploader :avatar, AvatarUploader      #carrierwave
+  mount_uploader :avatar, AvatarUploader      # carrierwave
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :identities, dependent: :destroy
-  has_many :followingships, foreign_key: :following_id, class_name: "Follow", dependent: :destroy
+  has_many :followingships, foreign_key: :following_id, class_name: 'Follow', dependent: :destroy
   has_many :fans, through: :followingships, source: :fan
-  has_many :fanships, foreign_key: :fan_id, class_name: "Follow", dependent: :destroy
+  has_many :fanships, foreign_key: :fan_id, class_name: 'Follow', dependent: :destroy
   has_many :followings, through: :fanships, source: :following
-  #建立使用者與對話關聯
+  # 建立使用者與對話關聯
   has_many :sender_chats, foreign_key: :sender_id, class_name: 'Chat'
   has_many :recipient_chats, foreign_key: :recipient_id, class_name: 'Chat'
   has_many :messages
 
-  scope :not_self, -> (current_user){ where.not(id: current_user.id) }
-  scope :find_by_keyword, -> (keyword){ where(["nick_name LIKE ? OR email LIKE ?", "%#{keyword}%", "%#{keyword}%"]) }
+  scope :not_self, ->(current_user) { where.not(id: current_user.id) }
+  scope :find_by_keyword, ->(keyword) { where(['nick_name LIKE ? OR email LIKE ?', "%#{keyword}%", "%#{keyword}%"]) }
 
-  def already_followed?(current_user)                  #檢查自己是否已經追蹤對方
-    self.fans.include?(current_user)
+  # 建立user與直播房的關聯
+  has_many :rooms
+
+  def already_followed?(current_user) # 檢查自己是否已經追蹤對方
+    fans.include?(current_user)
   end
 
   def self.from_omniauth(auth, signed_in_resource = nil)
     # 1. 搜尋用 google, fb 登入過的 user (identity 紀錄的 user)
     identity = Identity.find_for_oauth(auth)
-    user = signed_in_resource ? signed_in_resource : identity.user
+    user = signed_in_resource || identity.user
 
     # 2. 搜尋 google, fb 的信箱是否註冊過
-    if user.nil?
-      user = User.where(email: auth.info.email).first
-    end
+    user = User.where(email: auth.info.email).first if user.nil?
 
     # 3. 建立新 user
     if user.nil?
@@ -43,7 +44,7 @@ class User < ApplicationRecord
         nick_name: auth.info.email.split('@')[0].capitalize,
         email: auth.info.email,
         avatar: auth.info.image,
-        password: Devise.friendly_token[0,20]
+        password: Devise.friendly_token[0, 20]
       )
       user.save!
     end
@@ -53,6 +54,6 @@ class User < ApplicationRecord
       identity.save!
     end
 
-    return user
+    user
   end
 end
