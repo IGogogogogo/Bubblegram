46c47c0c07ee87591ad7d74bf321aed31e7ef823
gn2481 <gn2481@gmail.com>
2020-09-15 21:12:06 +0800

    add_rooms_view

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,10 @@
 class RoomsController < ApplicationController
   # 直播間的rooms
   before_action :find_room, only: %i[show play destroy]
+  def index
+    @rooms = Room.all
+  end
+
   def show
     opentok = OpenTok::OpenTok.new(ENV['vonage_api_key'], ENV['vonage_secret'])
     @token ||= opentok.generate_token(@room.session_id)
@@ -8,12 +12,12 @@ class RoomsController < ApplicationController
 
   def create
     @room = current_user.rooms.create(name: current_user.nick_name)
-    redirect to play_room_path(@room)
+    redirect_to play_room_path(@room)
   end
 
   def play
     opentok = OpenTok::OpenTok.new(ENV['vonage_api_key'], ENV['vonage_secret'])
-    @token ||= opentok.generate_token(@room.session_id)
+    @token ||= opentok.generate_token(@room.vonage_session_ID)
   end
 
   def destory
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
-
+  get '/rooms', to: 'rooms#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :users, only: %i[show edit update] do
-    resource :follows, only: %i[create destroy]
+  resources :users, only: [:show, :edit, :update] do
+    resource :follows, only: [:create, :destroy]
     member do
       get :fans
       get :followings
     end
     # 產生show create destory 與 play 路徑
-    resources :rooms, only: %i[show create destroy] do
-      member do
-        get :play
-      end
-    end
+    resources :rooms, only: [:create]
   end
 
+  resources :rooms, only: [:show, :destory] do
+    member do
+      get :play
+    end 
+  end
   resources :searches, only: [:index] do
     collection do
       get :search
@@ -28,7 +29,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
   end
 
-  resources :chats, only: %i[index show create destroy] do
+  resources :chats, only: [:index, :show, :create, :destroy] do
     resources :messages, only: [:create]
   end
 end
