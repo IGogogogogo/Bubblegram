17d5343289755cc567040fd9511fdc2a4e5ca2cf
Gavin Liu <du20783@gmail.com>
2020-09-16 16:02:18 +0800

    delete pages welcome

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:welcome]
 
   def index
     users = User.viewable_users(current_user)
     @posts = Post.includes(:user).viewable_posts(users).order("created_at DESC").limit(20)
-  end
-
-  def welcome
   end
 end
