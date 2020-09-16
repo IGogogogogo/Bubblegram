aadf54bf13c90c81690bf93a8d6c6f79bb9cd4b2
陳柏蓉 <jerry19920702@gmail.com>
2020-09-17 00:34:35 +0800

    fix_conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_024947) do
+ActiveRecord::Schema.define(version: 2020_09_15_090116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,10 +152,7 @@ ActiveRecord::Schema.define(version: 2020_09_12_024947) do
   add_foreign_key "favourites", "posts"
   add_foreign_key "favourites", "users"
   add_foreign_key "identities", "users"
-<<<<<<< HEAD
-  add_foreign_key "stories", "users"
-=======
   add_foreign_key "messages", "chats"
   add_foreign_key "messages", "users"
->>>>>>> develop
+  add_foreign_key "stories", "users"
 end
