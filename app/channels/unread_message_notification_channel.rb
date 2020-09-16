c4d4cf92f4c873a8a97ed6f80ed69f80fa98a334
will <wye139629@gmail.com>
2020-09-16 15:59:10 +0800

    create unread_message_channel

---
+++ app/channels/unread_message_notification_channel.rb
@@ -0,0 +1,9 @@
+class UnreadMessageNotificationChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "unread_message_notification_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
