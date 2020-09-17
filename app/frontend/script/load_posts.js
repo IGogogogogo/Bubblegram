98487f1d453f8e40096ec29e2f53dffaf1099681
Gavin Liu <du20783@gmail.com>
2020-09-17 21:43:32 +0800

    fix load post scroll event

--- app/frontend/script/load_posts.js
+++ app/frontend/script/load_posts.js
@@ -1,6 +1,6 @@
 import Rails from '@rails/ujs'
 document.addEventListener("turbolinks:load", () => {
-  const postLoadTarget = document.querySelector(".post-load-target")
+  const postLoadPage = document.querySelector(".post-load-page")
   const postNav = document.querySelector(".posts-nav")
   const postImg = document.querySelector(".post-img")
   const tagImg = document.querySelector(".tag-img")
@@ -9,7 +9,7 @@ document.addEventListener("turbolinks:load", () => {
   let myPage = 1
   // let type = ""
 
-  if (postLoadTarget) { postLoadTarget.addEventListener("scroll", loadPosts) }  //加入捲軸滾動事件
+  if (postLoadPage) { postLoadPage.addEventListener("scroll", loadPosts) }  //加入捲軸滾動事件
 
   if (postNav) { postNav.addEventListener("click", (e) => switchPosts(e)) }  //user show 個人/tag貼文切換
 
@@ -31,10 +31,10 @@ document.addEventListener("turbolinks:load", () => {
     }
   }
 
-  const scrollToBottom = () => { document.body.scrollHeight - window.innerHeight - 2 <= document.body.scrollTop }
+  function scrollToBottom() { return postLoadPage.scrollHeight - window.innerHeight - 2 <= postLoadPage.scrollTop }
 
   function loadPosts() {                           //滑動到畫面底部會請求載入更多post
-    if (scrollToBottom) {
+    if (scrollToBottom()) {
       // console.log("loadPosts.........................")
       page += 1
       const user_id = document.location.href.split("users/")[1]  //從目前網址取得params user id
@@ -47,20 +47,26 @@ document.addEventListener("turbolinks:load", () => {
         url: url,
         type: "get",
         success: function(data) {
+          console.log("success：", url)
           const postsEl = data.querySelector("body").innerHTML
+          const postLoadTarget = document.querySelector(".post-load-target")
 
           if (type == "following_posts" || type == "my_posts") {
-            document.querySelector(".post-load-target").innerHTML += postsEl
+            postLoadTarget.innerHTML += postsEl
           } else if (type == "post_img") {
             postImg.innerHTML += postsEl
           } else if (type == "tag_img") {
             tagImg.innerHTML += postsEl
           }
 
-          if (postsEl == "") { postLoadTarget.removeEventListener("scroll", loadPosts) } //沒有新資料時移除事件監聽
+
+          if (postsEl == "") {   //沒有新資料時移除事件監聽
+            postLoadPage.removeEventListener("scroll", loadPosts)
+          }
         },
         error: function(errors) {
           console.log(errors)
+          window.location.href = "/users/sign_in"
         }
       })
       // console.log("send rails ajax resquest!!!!!!!!!!")
@@ -88,6 +94,6 @@ document.addEventListener("turbolinks:load", () => {
       page = tagPage
     }
 
-    postLoadTarget.addEventListener("scroll", loadPosts)
+    postLoadPage.addEventListener("scroll", loadPosts)
   }
 })
