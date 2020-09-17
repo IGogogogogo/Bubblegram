94b6fb845412f330ae506aecfd454e874747f378
陳柏蓉 <jerry19920702@gmail.com>
2020-09-17 17:20:47 +0800

    add_Gasp_loginpage

--- app/frontend/script/index.js
+++ app/frontend/script/index.js
@@ -6,8 +6,11 @@ import "@fortawesome/fontawesome-free/js/all";
 import "./preview_photo"
 import "./bubble"
 import "./chat_message_view"
+import gsap from "gsap";
+
 
 window.$ = $
+window.gsap = gsap
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
