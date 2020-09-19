174263a87a431daa80f47f678c9d291e6f709bbb
Guest5x <guest5x@Hsuande-MacBook-Air.local>
2020-09-19 12:24:57 +0800

    add stoyies to pages index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def index
-    @users = User.viewable_users(current_user)
-    @posts = Post.includes(:user, :thumbs_up_users).viewable_posts(@users).order("created_at DESC").limit(20)
+    @following_users = current_user.followings.order("created_at DESC")
+    viewable_users = User.viewable_users(current_user)
+    @posts = Post.includes(:user, :thumbs_up_users).viewable_posts(viewable_users).order("created_at DESC").limit(20)
   end
 end
