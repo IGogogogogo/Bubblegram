279bbcaee935577e7c5e4120b69d95be75a6c08b
gn2481 <gn2481@gmail.com>
2020-09-22 03:12:16 +0800

    1. 修改i18n  部署失敗問題 2. 修改_story  user.avatar => user[:user] 3. 引入capistrano 相關gem in capfile.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_09_20_105628) do
 
   # These are extensions that must be enabled in order to support this database
@@ -55,15 +54,6 @@ ActiveRecord::Schema.define(version: 2020_09_20_105628) do
     t.index ["recipient_id", "sender_id"], name: "index_chats_on_recipient_id_and_sender_id", unique: true
     t.index ["recipient_id"], name: "index_chats_on_recipient_id"
     t.index ["sender_id"], name: "index_chats_on_sender_id"
-  end
-
-  create_table "collections", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_collections_on_post_id"
-    t.index ["user_id"], name: "index_collections_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -175,8 +165,6 @@ ActiveRecord::Schema.define(version: 2020_09_20_105628) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "collections", "posts"
-  add_foreign_key "collections", "users"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "favourites", "posts"
