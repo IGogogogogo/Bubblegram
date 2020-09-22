3231869eb6a9789852acd61d2ac26f505964efe6
dsfwoo <67670753+dsfwoo@users.noreply.github.com>
2020-09-22 18:10:07 +0800

    Delete favourites_count.js
    刪除多餘的 js 檔案

--- app/frontend/script/favourites_count.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('turbolinks:load', function (e) {
-  let post_id = this.data.get('id')
-  let num = document.querySelectorAll('.thumb-up strong')
-  num.forEach(function (e) {
-    console.log(e.target.post_id)
-  })
-})
