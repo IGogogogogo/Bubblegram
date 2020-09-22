4680598d1ed358db7fc75c24d3e4690045b5d325
Guest5x <guest5x@Hsuande-MacBook-Air.local>
2020-09-22 16:03:40 +0800

    stope

--- app/frontend/script/stories.js
+++ app/frontend/script/stories.js
@@ -2,24 +2,23 @@
 // 再將ＩＤ塞進刪除鈕
 // 後端刪除，要有一個contrallor
 
-
+const Rails = require("@rails/ujs")
 
 document.addEventListener("turbolinks:load", () => {
-
   const storyDelete = document.getElementById('story_delete')
 
   if(!storyDelete){return}
 
+  Rails.ajax({
+    url: "/",
+    type: "delete"
+    success: function(data){
+
+    }
+  })
+
+
   storyDelete.addEventListener("click", () => {
-    // const allStories = document.querySelectorAll(".story-img")
-    // console.log(allStories)
-
-    // const active = document.querySelectorAll(".active")
-    // console.log(active)
-
-    // active.classList.remove("active")
-    // allStories.forEach(() => {})
-
     const $activeImg = document.querySelector('.carousel-inner .story-img.active')
     if ($activeImg) $activeImg.remove()
     const $firstImg = document.querySelector('.carousel-inner .story-img')
