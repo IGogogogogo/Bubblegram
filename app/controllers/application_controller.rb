527fca8fe5541d7c91c933bb5b6e22874dae0fa2
Gavin Liu <du20783@gmail.com>
2020-09-23 15:27:46 +0800

    include and install pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,15 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  # helper_method :already_followed
+  after_action :verify_authorized, except: :index
+  # after_action :verify_policy_scoped, only: :index
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :description])
   end
-
-  # def already_followed(user)                  #檢查自己是否已經追蹤對方
-  #   user.fans.include?(current_user)
-  # end
-
-  # def default_url_options(options = {})
-  #   host_options = if Rails.env.development?
-  #     { host: request.headers["HTTP_X_ORIGINAL_HOST"] }
-  #   else
-  #     {}
-  #   end
-  #   options.merge(host_options)
-  # end
 end
