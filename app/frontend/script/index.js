278c0777c28377cb22bb2a728709ab7fd789f55f
Guest5x <guest5x@Hsuande-MacBook-Air.local>
2020-09-23 15:10:28 +0800

    ture_delete

--- app/frontend/script/index.js
+++ app/frontend/script/index.js
@@ -15,7 +15,7 @@ import "./chat_message_view"
 import "./post_form.js"
 import "./load_posts.js"
 import "./owl_carousel.js"
-import gsap from "gsap"
+import gsap from "gsap";
 
 window.gsap = gsap
 
--- app/frontend/script/stories.js
+++ app/frontend/script/stories.js
@@ -3,26 +3,48 @@
 // 後端刪除，要有一個contrallor
 
 const Rails = require("@rails/ujs")
+import Swal from 'sweetalert2'
 
 document.addEventListener("turbolinks:load", () => {
   const storyDelete = document.getElementById('story_delete')
 
+  const userId = document.location.href.split("users/")[1] // 取得url的params user id
+  const storyElementId = document.querySelector(".story_id")
+  const storyId = storyElementId.getAttribute("story_id")
+  // console.log(userId)
+  // console.log(storyId)
+
+  let url = `/users/${userId}/${storyId}`
+  // console.log(url)
+
   if(!storyDelete){return}
-
-  Rails.ajax({
-    url: "/",
-    type: "delete"
-    success: function(data){
-
-    }
-  })
-
 
   storyDelete.addEventListener("click", () => {
     const $activeImg = document.querySelector('.carousel-inner .story-img.active')
-    if ($activeImg) $activeImg.remove()
-    const $firstImg = document.querySelector('.carousel-inner .story-img')
-    if ($firstImg) $firstImg.classList.add('active')
-  })
 
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        if ($activeImg) $activeImg.remove()
+        const $firstImg = document.querySelector('.carousel-inner .story-img')
+        if ($firstImg) $firstImg.classList.add('active')
+
+        Rails.ajax({
+          url: url,
+          type: "DELETE",
+          success: function(data){
+            // console.log("success:", url)
+          }
+        })
+      }
+    })
+  })
 })
