c939a942201305b911d116f01f14ba252bf4d1f9
Gavin Liu <du20783@gmail.com>
2020-09-25 21:16:44 +0800

    update action authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  after_action :verify_authorized, except: :index
+  # after_action :verify_authorized, except: :index
   # after_action :verify_policy_scoped, only: :index
   helper_method :owner?
 
