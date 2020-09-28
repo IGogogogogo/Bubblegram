46ea7652ea04cc1aff14c0ac2c107a613c5a705b
will <wye139629@gmail.com>
2020-09-28 16:04:30 +0800

    deleted stop_all_stream in chat_channel.rb and replace chat_id to chatroom_id

--- app/channels/unread_message_notification_channel.rb
+++ app/channels/unread_message_notification_channel.rb
@@ -10,7 +10,7 @@ class UnreadMessageNotificationChannel < ApplicationCable::Channel
   end
 
   def new_message(data)
-    redis.rpush("#{data["chat_id"]}_#{data["user_id"]}_new_message",data.to_json)
+    redis.rpush("#{data["chatroom_id"]}_#{data["user_id"]}_new_message",data.to_json)
     #使用redis將新的訊息存起來
     # redis 是一個 key 跟 value 形式 key的設計是紀錄這則訊息 是哪一個聊天室 以及 哪一個人？
     # value 是從unread_message_channel.js 傳過來的那則訊息 轉成json格式（為了之後要做處理方便）
