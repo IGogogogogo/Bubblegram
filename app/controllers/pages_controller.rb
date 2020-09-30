b02246efe4b26de84a41ce08aacda3112720525f
yuan <gn2481@gmail.com>
2020-09-30 19:21:49 +0800

    fix/story_and_room_collection

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,12 +12,14 @@ class PagesController < ApplicationController
 
     # 將所需要的結果組成陣列，塞給render的collection
     def build_results
+      @stories_user = @following_users.select{|user| user.exist_room? || user.exist_story?}
+
       [ # 若current_user 有限時動態，轉到show，若沒有，則到new
         { user: current_user, path: current_user.exist_story? ? user_stories_path(user_id: current_user.nick_name) : new_user_story_path(user_id: current_user.nick_name) }
       ].concat(
         # 若following_users 有直播，轉到直播的show，若沒有，則到限時動態
-        @following_users.map do |user|
-          { user: user, path: user.exist_room? ? room_path(id: user.room.slug) : user_stories_path(user) }
+        @stories_user.map do |user|
+          { user: user, path: user.exist_room? ? room_path(id: user.room.id) : user_stories_path(user) }
         end
       )
     end
