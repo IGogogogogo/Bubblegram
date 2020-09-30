262c42bd99fb46125f55f8d02011238102989853
Guest5x <guest5x@Hsuande-MacBook-Air.local>
2020-09-30 12:50:55 +0800

    stroy_del_createdtime_done

--- app/frontend/script/stories.js
+++ app/frontend/script/stories.js
@@ -1,63 +1,62 @@
-// 先抓到圖片的ＩＤ
-// 再將ＩＤ塞進刪除鈕
-// 後端刪除，要有一個contrallor
-
-const Rails = require("@rails/ujs")
-import Swal from 'sweetalert2'
+import Rails from "@rails/ujs"
+import Swal from "sweetalert2"
 
 document.addEventListener("turbolinks:load", () => {
-  const funBtn = document.querySelector('.fun-btn');
-  const modalFun = document.querySelector('.modal-fun');
-  const modalDel = document.querySelector('.modal-del');
-  const modalCancel = document.querySelector('.modal-cancel')
+  const storiesSection = document.querySelector('.stories')
+  if(!storiesSection){ return }
+  document.querySelector(".container").style.padding = "0"
 
-  const storyDelete = document.getElementById('story_delete')
-  const userId = document.location.href.split("users/")[1] // 取得url的params user id
-  const storyElementId = document.querySelector(".story_id")
-  const storyId = storyElementId.getAttribute("story_id")
-  // console.log(userId)
-  // console.log(storyId)
-  let url = `/users/${userId}/${storyId}`
-  // console.log(url)
+  ////刪除story
+  const headerBtn = document.querySelector('.header-btn')
+  if(!headerBtn){return}
 
-  funBtn.addEventListener('click', function(){
-    modalFun.classList.add('open')
-  })
-  modalCancel.addEventListener('click', function(){
-    modalFun.classList.remove('open')
-  })
-
-  // 真刪除資料庫story
-  if(!storyDelete){return}
-  storyDelete.addEventListener("click", (e) => {
-    const $activeImg = document.querySelector('.carousel-inner .story-img .active')
+  headerBtn.addEventListener("click", (e) => {
     e.preventDefault()
+    $("#stories-carousel").carousel('pause')
+
+    const userId = document.querySelector(".stories").dataset.user // 取得 user id
+    const $activeImg = document.querySelector('.story-img.active')
+    const storyId = $activeImg.dataset.story
+    const url = `/users/${userId}/stories/${storyId}`
+
     Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
+      title: '你確定要刪除嗎？',
+      // text: "You won't be able to revert this!",
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#d33',
       cancelButtonColor: '#3085d6',
-      confirmButtonText: 'Yes, delete it!'
+      confirmButtonText: '刪除',
+      cancelButtonText: '取消'
     }).then((result) => {
       if (result.isConfirmed) {
-
+        //前端畫面刪除
         if ($activeImg) $activeImg.remove()
         const $firstImg = document.querySelector('.carousel-inner .story-img')
         if ($firstImg) $firstImg.classList.add('active')
 
+        //資料庫刪除
         Rails.ajax({
           url: url,
           type: "DELETE",
-          success: function(data){
-            // console.log("success:", url)
+          success: function(){
+            console.log("success:", url)
           }
         })
       }
     })
   })
 
+  ///顯示時間
+  ////////輪播的事件
+  $('#stories-carousel').on('slide.bs.carousel', function () {
+    // console.log("change")
 
-
+    const $activeImg = document.querySelector('.story-img.active')
+    const storyId = $activeImg.dataset.story
+    Array.from(document.querySelectorAll(".member-time")).forEach((el) => {
+      el.style.display = "none"
+    })
+    document.querySelector(`.member-time.story-${storyId}`).style.display = "block"
+  })
 })
