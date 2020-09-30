8a1f973f8ef56476f904a7e191922784895e9911
will <wye139629@gmail.com>
2020-09-30 13:42:17 +0800

    create partial and fix message/create action and add form reset

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,26 +2,27 @@ class MessagesController < ApplicationController
   def create
     live_stream_room = Room.find(params[:room_id]) if params[:room_id]
     chat_room = Chat.find(params[:chat_id]) if params[:chat_id]
-    @live_stream_message = live_stream_room.messages.create(params_message)
-    @user = User.find(@live_stream_message.user_id)
+    if live_stream_room
+      @live_stream_message = live_stream_room.messages.create(params_message)
+      @user = User.find(@live_stream_message.user_id)
 
-    SendLiveStreamMessageJob.perform_later(@live_stream_message, @user)
+      SendLiveStreamMessageJob.perform_later(@live_stream_message, @user)
+    else
+      @opposed_user = chat_room.opposed_user(current_user).id
+      @chat_message = chat_room.messages.create(params_message)
 
-    return if !chat_room
-    @opposed_user = chat_room.opposed_user(current_user).id
-    @chat_message = chat_room.messages.create(params_message)
+      SendMessageJob.perform_later(@chat_message, new_message_counts)
 
-    SendMessageJob.perform_later(@chat_message, new_message_counts)
+      if !is_online_channel_connect?  #判斷對方使用者有沒有在線上
+       redis.rpush("#{@chat_message.chatroom_id}_#{@chat_message.user_id}_new_message", @chat_message.to_json) #下線將訊息存為新訊息
+      end
 
-    if !is_online_channel_connect?  #判斷對方使用者有沒有在線上
-      redis.rpush("#{@message.chatroom_id}_#{@message.user_id}_new_message", @message.to_json) #下線將訊息存為新訊息
+      if !is_unread_channel_connect? && is_online_channel_connect? #如果對方使用者在線上且通知channel斷線（代表正在換頁）
+      redis.rpush("#{@chat_message.chatroom_id}_#{@chat_message.user_id}_new_message", @chat_message.to_json) #將訊息存為新訊息
+      end
+      # @message.save
+      # redirect_to chat_path(chat_room)
     end
-
-    if !is_unread_channel_connect? && is_online_channel_connect? #如果對方使用者在線上且通知channel斷線（代表正在換頁）
-      redis.rpush("#{@message.chatroom_id}_#{@message.user_id}_new_message", @message.to_json) #將訊息存為新訊息
-    end
-    # @message.save
-    # redirect_to chat_path(chat_room)
   end
 
 private
@@ -38,7 +39,7 @@ private
   end
 
   def new_message_counts
-    redis.lrange("#{@message.chatroom_id}_#{@message.user_id}_new_message",0,-1).length
+    redis.lrange("#{@chat_message.chatroom_id}_#{@chat_message.user_id}_new_message",0,-1).length
   end
 
 end
