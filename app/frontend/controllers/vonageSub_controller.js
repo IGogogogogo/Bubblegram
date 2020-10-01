e433f25d390c7b0de7841bd2114779aa03bd7ab3
will <wye139629@gmail.com>
2020-10-01 21:15:47 +0800

    remove message_area when live_stream is over and modified ub template

--- app/frontend/controllers/vonageSub_controller.js
+++ app/frontend/controllers/vonageSub_controller.js
@@ -16,6 +16,7 @@ export default class extends Controller {
     console.log('sub disconnect');
     if (this.session) { this.session.disconnect() }
     document.querySelector('.end-notice').classList.remove('d-none')
+    document.querySelector(".room-message").remove()
     // this.session.unsubscribe(subscriber)
   }
 
