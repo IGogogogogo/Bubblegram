9c4b6f48d9127124d7ba2d089d75169dadcf1811
Gavin Liu <du20783@gmail.com>
2020-10-02 22:54:06 +0800

    add upload file job

---
+++ app/jobs/upload_file_job.rb
@@ -0,0 +1,6 @@
+class UploadFileJob < ApplicationJob
+  queue_as :default
+
+  def perform()
+  end
+end
---
+++ test/jobs/upload_file_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UploadFileJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
