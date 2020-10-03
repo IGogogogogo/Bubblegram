0b5887d9b49b33b223d44d3a80607aaedfd6b224
yuan <gn2481@gmail.com>
2020-10-03 14:42:28 +0800

    add_default_picture_to_pages_index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def index
     per_count = 20
     @following_users = current_user.followings.order("created_at DESC")
-    @viewable_users = User.viewable_users(current_user)
+    viewable_users = User.viewable_users(current_user)
     @results = build_results
     @posts = Post.includes(:user, :thumbs_up_users).viewable_posts(viewable_users).order("created_at DESC").limit(per_count)
     @has_more_posts = (@posts.count >= per_count)
