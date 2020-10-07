77e6938a153b848c234c58bfdf941351b3282aee
will <wye139629@gmail.com>
2020-10-07 12:39:01 +0800

    use job to broadcast user oline

--- app/channels/user_online_channel.rb
+++ app/channels/user_online_channel.rb
@@ -2,7 +2,7 @@ class UserOnlineChannel < ApplicationCable::Channel
   def subscribed
     stream_from "user_online_channel"
     #使用者一上線就廣播
-    ActionCable.server.broadcast "user_online_channel", {user_id: current_user.id, is_online: true, online: "目前在線上"}
+    UserOnlineNoticeJob.perform_later(current_user)
     #並使用redis紀錄使用者上線
     redis.set("user_#{current_user.id}_online", "1")
   end
@@ -10,13 +10,14 @@ class UserOnlineChannel < ApplicationCable::Channel
   def unsubscribed
     #關掉視窗或登出將狀態刪除並廣播出去
     redis.del("user_#{current_user.id}_online")
-    ActionCable.server.broadcast "user_online_channel", {user_id: current_user.id, is_online: false}
+    UserOnlineNoticeJob.perform_later(current_user)
     # Any cleanup needed when channel is unsubscribed
   end
 
   private
 
   def redis
-    Redis.new
+    redis = Redis.new unless redis
+    redis
   end
 end
