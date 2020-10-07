b9830197929a5c7e3eb0820507a476b630f2ef71
will <wye139629@gmail.com>
2020-10-07 15:12:31 +0800

    establish footer chat_notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   # after_action :verify_authorized, except: :index
   # after_action :verify_policy_scoped, only: :index
   helper_method :owner?
+  helper_method :has_any_chatroom_new_message?
 
   def owner?(record)
     record.user == current_user
@@ -19,6 +20,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def has_any_chatroom_new_message?
+    redis.lrange("#{current_user.id}_chat_notice",0,-1).present?
+  end
   protected
 
   def configure_permitted_parameters
