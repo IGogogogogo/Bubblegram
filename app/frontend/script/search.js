2dde95d2b87a776e8e2125a49c30521c462c828c
Gavin Liu <du20783@gmail.com>
2020-10-08 11:00:40 +0800

    add random post for search index

--- app/frontend/script/search.js
+++ app/frontend/script/search.js
@@ -3,17 +3,30 @@ import Rails from '@rails/ujs'
 document.addEventListener("turbolinks:load", () => {
   const searchForm = document.querySelector(".search-form")
   const searchInput = document.querySelector(".search-form input")
+  const postLoadPage = document.querySelector(".post-load-page")
   let chatUserList = document.querySelector(".chat-user-list")
-  if (searchForm) {
-    ///Rails.fire 使用 ajax 方式傳送資料
-    searchForm.addEventListener("keyup", () => {
-      Rails.fire(searchForm, "submit")
-      if(chatUserList){
-        chatUserList.style.display = "none"
-      }
-      if(chatUserList && searchInput.value === ''){
+
+  if (!searchForm) return
+
+  if (postLoadPage) {
+    document.querySelector(".container").style.paddingTop = "20px"
+  }
+
+  searchForm.addEventListener("keyup", () => {
+    Rails.fire(searchForm, "submit")     ///Rails.fire 使用 ajax 方式傳送資料
+
+    if(chatUserList){
+      chatUserList.style.display = "none"
+      if(searchInput.value === ''){
         chatUserList.style.display = "block"
       }
+    }
+
+    if (postLoadPage) {
+      postLoadPage.style.display = "block"
+      if (searchInput.value != "") {
+        postLoadPage.style.display = "none"
+      }
+    }
   })
-  }
 })
