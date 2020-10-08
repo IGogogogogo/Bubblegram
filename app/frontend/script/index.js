263e118b7571c34f5a86ce91f9b1d7a72aa321cf
Gavin Liu <du20783@gmail.com>
2020-10-08 21:03:38 +0800

    add simple format for comment

--- app/frontend/script/index.js
+++ app/frontend/script/index.js
@@ -18,6 +18,7 @@ import "./footer_ disappear"
 import "./upload"
 import "./user_pic_upload"
 import "./sign_up"
+import "./reset_container.js"
 
 import Swiper, { Pagination } from 'swiper';
 import gsap from "gsap"
