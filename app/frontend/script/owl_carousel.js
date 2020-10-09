f5038fcbb3eeca9c6ec872624d9ce22811b6fe0e
陳柏蓉 <jerry19920702@gmail.com>
2020-10-09 23:10:47 +0800

    add_carousel_to_limit_video

--- app/frontend/script/owl_carousel.js
+++ app/frontend/script/owl_carousel.js
@@ -1,16 +1,31 @@
 document.addEventListener("turbolinks:load", () => {
   const dot = document.querySelector(".owl-dot")
-  if(dot){ return }         ///處理輪播點重複出現問題
+  if (dot) { return }         ///處理輪播點重複出現問題
 
-  $('.owl-carousel').owlCarousel({
+  $('.post-pic-carousel').owlCarousel({
     loop: false,
     margin: 0,
     nav: false,
     animateOut: 'fadeOut',
-    responsive:{
-      0:{
-          items:1
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  });
+
+
+  $('.limit-video-carousel').owlCarousel({
+    loop: false,
+    margin: 0,
+    nav: false,
+    dots: false,
+    animateOut: 'fadeOut',
+    responsive: {
+      0: {
+        items: 5
       }
     }
   })
+
 })
