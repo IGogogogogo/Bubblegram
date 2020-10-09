250a2c4c0796d98ad3b3fc76a364700d190daa10
陳柏蓉 <jerry19920702@gmail.com>
2020-10-09 22:39:34 +0800

    fix_login_page

--- app/frontend/script/index.js
+++ app/frontend/script/index.js
@@ -17,6 +17,7 @@ import "./active"
 import "./footer_ disappear"
 import "./upload"
 import "./user_pic_upload"
+import "./validation"
 
 import Swiper, { Pagination } from 'swiper';
 import gsap from "gsap"
