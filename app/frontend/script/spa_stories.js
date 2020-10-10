e2d5b4a63f368b92dcaa19701c7a116b40758455
Gavin Liu <du20783@gmail.com>
2020-10-10 19:04:09 +0800

    fix to anothor user error

--- app/frontend/script/spa_stories.js
+++ app/frontend/script/spa_stories.js
@@ -4,22 +4,8 @@ import Swal from "sweetalert2"
 document.addEventListener("turbolinks:load", () => {
   console.log("123")
   const carouselTime = 1000000 ////限動輪播時間
-  // 功能:
-  // 1. 可以載入限動
-  // 2. 可以輪播（自動，按鈕，滑動）
-  // 3. 可以換上下使用者
-  // 4. 可以刪除
-  // 5. 有白線提示目前限動
-
-
-  // 步驟：
-  // 請求 [當前使用者] 限動
-  //   [id]
-  //   [picture]
-  //   [create time]
-  //   [限動數量]
-
   const storiesSection = document.querySelector(".stories")
+
   if(!storiesSection) return
   const userName = storiesSection.dataset.userName
 
@@ -28,6 +14,9 @@ document.addEventListener("turbolinks:load", () => {
 
   ////請求個人現動json
   function requestStories(name, index=0) {
+    if(name == "null") {
+      document.location.pathname ="/"
+    }
     const url = `/stories.json?user=${name}`
 
     Rails.ajax({
@@ -38,16 +27,15 @@ document.addEventListener("turbolinks:load", () => {
         storiesSection.dataset.userName = data.stories.user_name
         storiesSection.dataset.prevUser = data.stories.prev_user
         storiesSection.dataset.nextUser = data.stories.next_user
+        storiesSection.dataset.storiesCount = data.stories.stories_count
         storiesSection.dataset.storyIndex = index
 
         renderStories(data.stories)
 
-        if (index == "last") {
-          console.log(data.stories.main.length)
+        if (index == "last") {    ////如果是請求上一個使用者限動會跳到他最後的現動
           index = data.stories.main.length - 1
           $('.owl-carousel').trigger("to.owl.carousel", [index, 1])
         }
-
       },
       error: function(errors) {
         console.log(errors)
@@ -70,7 +58,8 @@ document.addEventListener("turbolinks:load", () => {
     carouselStart()
     createDots()
     addCarouselChangeEvent()
-    replacePrevUserWhenFirstStory()
+    document.querySelector(".owl-prev").addEventListener("click", toPrevUserLastStory)
+    // document.querySelector(".owl-next").addEventListener("click", toNextUserStories)
   }
 
   /////加入 使用者資料和現動時間
@@ -129,43 +118,40 @@ document.addEventListener("turbolinks:load", () => {
 
       setTimeout(() => {  //////用setTimeout 才找得到 active item
         const storyActive = storiesSection.querySelector(".owl-item.active div")
-        const lastStory = document.querySelector(".owl-stage").lastChild.firstChild
-        // console.log(storyActive.dataset)
 
-        storiesSection.dataset.storyIndex = storyActive.dataset.storyIndex
-        console.log(storyActive.dataset.storyIndex)
-        console.log("index: " + storiesSection.dataset.storyIndex)
-
+        storiesSection.dataset.storyIndex = storyActive.dataset.storyIndex   ////設定index 用於判斷要不要請求另一個使用者限動
         changeStoryTime(storyActive.dataset)
+        const isLastStory = Number(storyActive.dataset.storyIndex) + 1 == Number(storiesSection.dataset.storiesCount)
 
-        if(storyActive == lastStory){
-          document.querySelector(".owl-next").addEventListener("click", () => replaceToNextUserStories())
-          setTimeout(() => replaceToNextUserStories(), carouselTime);
+        if(isLastStory){
+          document.querySelector(".owl-next").addEventListener("click", toNextUserStories)
         }
       }, 0)
     })
   }
 
   /////替換成下個使用者限動
-  function replaceToNextUserStories() {
-    console.log(storiesSection.dataset)
-    const nextUser = storiesSection.dataset.nextUser
-    requestStories(nextUser)
+  function toNextUserStories() {
+    // console.log(storiesSection.dataset)
+    const storyActive = storiesSection.querySelector(".owl-item.active div")
+    const isLastStory = Number(storyActive.dataset.storyIndex) + 1 == Number(storiesSection.dataset.storiesCount)
+
+    if (isLastStory){
+      const nextUser = storiesSection.dataset.nextUser
+      requestStories(nextUser)
+    }
+
+    document.querySelector(".owl-next").removeEventListener("click", toNextUserStories)
   }
 
   ////如果是第一則限動，按上一個限動時替換成上一個使用者最後的限動
-  function replacePrevUserWhenFirstStory() {
-    console.log(storiesSection.dataset.storyIndex)
-    const storyIndex = storiesSection.dataset.storyIndex
-    const prevUserName = storiesSection.dataset.prevUser
-    console.log(storyIndex)
-    console.log(storiesSection.dataset)
-    console.log(prevUserName)
+  function toPrevUserLastStory() {
+    let storyIndex = storiesSection.dataset.storyIndex
+    let prevUserName = storiesSection.dataset.prevUser
     if (storyIndex == "0") {
-      console.log("true")
-      document.querySelector(".owl-prev").addEventListener("click", () => requestStories(prevUserName, "last"))
+      requestStories(prevUserName, "last")
+      document.querySelector(".owl-prev").removeEventListener("click", toPrevUserLastStory)
     }
-
   }
 
   //////////改變現動白線顏色
@@ -192,47 +178,6 @@ document.addEventListener("turbolinks:load", () => {
     info.style = "position: absolute;top: 40%;font-size: 50px;background-color: #000;"
     return info
   }
-
-
-  // 請求 [使用者與所有追蹤人名字]
-  //   所有人[nick name]
-
-  // 建立 [使用者] 限動
-  //   限動有 [id] 作為刪除判斷
-  //   當前現動有 [active]
-  //   限動外層加入 [user name]
-
-  // 到目前 [使用者] [最後的限動][按下一個]時
-  //   有[下個使用者]
-  //     有[下個使用者]限動
-  //       跳到[下個使用者][第一個]限動
-  //     沒有[下個使用者]限動
-  //       請求[下個使用者]限動
-  //       隱藏目前限動外層
-  //       建立[新使用者]限動
-  //       跳到[下個使用者][第一個]限動
-  //   沒有[下個使用者]
-  //     回首頁
-
-
-  // 目前是[使用者][第一個現動][按上一個]時
-  // 有[上個使用者]
-  //   有[上個使用者]限動
-  //     跳到[上個使用者][最後的]限動
-  //   沒有[上個使用者]限動
-  //     請求[上個使用者]限動
-  //     隱藏目前限動外層
-  //     建立[新使用者]限動
-  //     跳到[上個使用者][最後的]限動
-  // 沒有[上個使用者]
-  //   回首頁
-
-  // [使用者]是本人時
-  //   加入刪除選項
-
-  // 限動刪除時
-  //   取得id
-  //   刪除
 
   function cssSetting() {       ///設定限時動態頁面 css style
     document.querySelector(".container").style.padding = "0"
