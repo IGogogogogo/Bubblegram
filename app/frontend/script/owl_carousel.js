8475ed639faec72c545a3356f4e89fab8f15ebd0
Gavin Liu <du20783@gmail.com>
2020-10-11 15:12:41 +0800

    fix carousel prev page dots error

--- app/frontend/script/owl_carousel.js
+++ app/frontend/script/owl_carousel.js
@@ -1,6 +1,6 @@
 document.addEventListener("turbolinks:load", () => {
   ///////加入class用來判斷以執行過的輪播屬性
-  $('.owl-carousel').addClass('owl-carousel-loaded').owlCarousel({
+  $('.owl-carousel:not(.owl-carousel-loaded)').addClass('owl-carousel-loaded').owlCarousel({
     loop: false,
     margin: 0,
     nav: false,
