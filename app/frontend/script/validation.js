03399dfd464225bb3ba310ac7b5e12f246bf7753
陳柏蓉 <jerry19920702@gmail.com>
2020-10-12 17:18:03 +0800

    fix_validate_form

--- app/frontend/script/validation.js
+++ app/frontend/script/validation.js
@@ -1,6 +1,7 @@
 window.addEventListener('turbolinks:load', function () {
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.getElementsByClassName('needs-validation');
+  let forms = document.getElementsByClassName('needs-validation');
+  let passwordInp = document.querySelector('.password-input')
   // Loop over them and prevent submission
   Array.prototype.filter.call(forms, function (form) {
     form.addEventListener('submit', function (event) {
@@ -9,7 +10,6 @@ window.addEventListener('turbolinks:load', function () {
         event.stopPropagation();
       }
       form.classList.add('was-validated');
-
     }, false);
   });
 });
