db9c6b48bf354649833bdd7c5233be04a81ddac6
will <wye139629@gmail.com>
2020-10-13 11:24:06 +0800

    create visits count job and brodcast

--- app/channels/live_stream_visits.rb
+++
@@ -1,9 +0,0 @@
-class LiveStreamVisitsChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from ""
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
--- app/frontend/controllers/liveStreamVisits_controller.js
+++ app/frontend/controllers/liveStreamVisits_controller.js
@@ -3,15 +3,16 @@ import consumer from "../channels/consumer"
 
 export default class extends Controller {
   connect() {
-
-
+    console.log("js count")
+    let liveStreamVisitsController = this
     this.subscription = consumer.subscriptions.create(
       {
-        channel: "",
+        channel: "LiveStreamVisitsChannel",
+        room_id: liveStreamVisitsController.data.get("id")
       },
       {
         connected() {
-
+          // console.log("counts")
           // Called when the subscription is ready for use on the server
         },
         disconnected() {
