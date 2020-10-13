9b7ae015961c2c2f2ecf2d7ef0934d762be74d7e
Your Name <you@example.com>
2020-10-13 12:29:50 +0800

    repair_timeline

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,32 +2,6 @@ class StoriesController < ApplicationController
   before_action :find_story, only: [:show, :destroy]
 
   def index
-    # @user = User.find(params[:user_id])
-    # viewable_users = User.viewable_users(current_user)
-    # @viewable_users = viewable_users.map(&:nick_name)
-    # @stories = Story.where(user: viewable_users).includes(:user).order("user_id", "created_at DESC").group_by(&:user_id)    #產生hash key = user_id
-    # # @stories = Story.where(user: viewable_users).includes(:user).stories_oneday
-    # 去撈24hr內po的storiesa，測試時可以用5.second。
-    # @user = User.find(params[:user_id])
-    # @stories = @user.stories.includes(:user).order("created_at DESC")
-    # @viewable_users = [current_user].concat(current_user.followings.order("created_at DESC")).map(&:nick_name)
-
-    # @result = {
-    #   stories: @stories,
-    #   user: @user,
-    #   viewable_users: @viewable_users
-    # }
-    @user = User.find(params[:user_id])
-    viewable_user = [current_user].concat(current_user.followings.order("created_at DESC"))
-    # @viewable_user = viewable_user.map(&:nick_name)
-    @stories = viewable_user.map{ |user| user.stories.order("created_at DESC") }
-    @first_story_time = @user.stories.last.created_at
-
-    @result = {
-      stories: @stories,
-      user_name: @user.nick_name,
-      user_index: viewable_user.index(@user)
-    }
   end
 
   def load_stories
@@ -62,9 +36,7 @@ class StoriesController < ApplicationController
   end
 
   def new
-    # check_owner
     @story = current_user.stories.new
-    # authorize @story
   end
 
   def create
