e23744bc85f421feac973948ba1b6cefa8c47e46
Gavin Liu <du20783@gmail.com>
2020-10-14 00:18:20 +0800

    add default scope for story

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,14 +12,14 @@ class PagesController < ApplicationController
     # 將所需要的結果組成陣列，塞給render的collection
 
     def build_results
-      @stories_user = @following_users.select{|user| user.exist_room? || user.stories.stories_oneday != []}
+      @stories_user = @following_users.select{|user| user.exist_room? || user.exist_story?}
 
       [ # 若current_user 有限時動態，轉到show，若沒有，則到new
-        { user: current_user, path: current_user.stories.stories_oneday != [] ? stories_path(user: current_user.nick_name) : new_user_story_path(user_id: current_user.nick_name) }
+        { user: current_user, path: current_user.exist_story? ? stories_path(user: current_user.nick_name) : new_user_story_path(user_id: current_user.nick_name) }
       ].concat(
         # 若following_users 有直播，轉到直播的show，若沒有，則到限時動態
         @stories_user.map do |user|
-          { user: user, path: user.exist_room? ? room_path(id: user.room.slug) :  stories_path(user: user.nick_name) }
+          { user: user, path: user.exist_room? ? room_path(id: user.room.slug) : stories_path(user: user.nick_name) }
         end.sort{|a,b| a[:path].match?('stories') and !b[:path].match?('stories') ? 1 : 0}
       )
 
--- app/models/story.rb
+++ app/models/story.rb
@@ -5,7 +5,5 @@ class Story < ApplicationRecord
   # 資料驗證，picture欄位不得為空
 
   belongs_to :user
-  # socpe viewable_stories -> (users){ where(user_id: users) }
-
-  scope :stories_oneday, -> {where('created_at >= ?', Time.zone.now - 1.day)} # 找24hr的story
+  default_scope { where("created_at >= ?", Time.zone.now - 1.day) } # 找24hr的story
 end
