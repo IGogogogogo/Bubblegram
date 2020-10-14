57b2f64085035b8d4f9de53af4a065f12c680b3a
陳柏蓉 <jerry19920702@gmail.com>
2020-10-14 15:18:59 +0800

    add_buble_btn

--- app/frontend/script/active.js
+++ app/frontend/script/active.js
@@ -4,19 +4,25 @@ window.addEventListener('turbolinks:load', function () {
   let addBtn = document.querySelectorAll('.footer-btn')[2]
   let likeBtn = document.querySelectorAll('.footer-btn')[3]
   let profileBtn = document.querySelectorAll('.footer-btn')[4]
+  let btnBg = document.getElementById('btn-bg')
 
-  if(!homeBtn)return
+  if (!homeBtn) return
 
   if (location.href === homeBtn.href) {
     homeBtn.classList.add('active')
   } else if (location.href === searchBtn.href) {
     searchBtn.classList.add('active')
-  } else if (location.href === addBtn.href) {
-    addBtn.classList.add('active')
   } else if (location.href === likeBtn.href) {
     likeBtn.classList.add('active')
   } else if (location.href === profileBtn.href) {
     profileBtn.classList.add('active')
   }
 
+  addBtn.addEventListener('click', function (event) {
+    btnBg.classList.toggle('open')
+    addBtn.classList.add('active')
+    setTimeout(function () {
+      addBtn.classList.remove('active')
+    }, 100);
+  })
 })
