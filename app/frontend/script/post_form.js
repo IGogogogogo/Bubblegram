a086b2286a3e0f6231e691e209a18d3ffb543e60
Gavin Liu <du20783@gmail.com>
2020-10-15 21:03:56 +0800

    fix tag user invaild problem

--- app/frontend/script/post_form.js
+++ app/frontend/script/post_form.js
@@ -11,9 +11,9 @@ document.addEventListener("turbolinks:load", () => {
 
   if(tagMessage.textContent != ""){   ////controller 有 flash tag 時會有內容
     const tagLabel = postFormTag.querySelector("label").textContent
-    const errorMessageEl = document.createElement("small")
+    const errorMessageEl = document.createElement("span")
     errorMessageEl.textContent = tagMessage.textContent
-    errorMessageEl.classList = "text-danger"
+    errorMessageEl.style.color = "red"
     postFormTag.querySelector("label").appendChild(errorMessageEl)
     postFormTag.querySelector("label").classList = "d-flex justify-content-between"
     postFormTag.addEventListener("click", () => { ///移除錯誤訊息
