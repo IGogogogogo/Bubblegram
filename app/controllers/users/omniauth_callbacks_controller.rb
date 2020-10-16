d0fcdbad2c2fe26af5e015a4c93f1e1f29311aa6
yuan <gn2481@gmail.com>
2020-10-16 18:33:12 +0800

    fix/fix_vaildate_nick_name

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,4 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_action :authenticate_user!
   def google_oauth2
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"], current_user)
